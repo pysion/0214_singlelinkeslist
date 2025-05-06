80f58a0575b440384edaf516627ad59b7b538ed1
AdamRaditya <Adamradityaakhsan@gmail.com>
2025-05-06 11:25:30 +0700

    membuat program traverse

--- Singly_Linkedlist.cpp
+++ Singly_Linkedlist.cpp
@@ -81,6 +81,16 @@ class LinkedList
     }
     
     void traverse() {
-     
+        if (listEmpty()) {
+            cout << "\nList Kosong\n";
+        }
+        else {
+            cout << "\nData didalam list adalah :\n";
+            Node *currentNode = START;
+            while (currentNode != NULL) {
+                cout << currentNode->noMhs << endl;
+                currentNode = currentNode->next;
+            }
+        }
     }
 };
