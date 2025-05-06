8760d17a633c6903345227fd7346cc201753f9b7
AdamRaditya <Adamradityaakhsan@gmail.com>
2025-05-06 12:03:02 +0700

    membuat case 4

--- Singly_Linkedlist.cpp
+++ Singly_Linkedlist.cpp
@@ -139,7 +139,25 @@ int main () {
                 case '3': {
                     mhs.traverse();
                 }
-                
+                break;
+                case '4': {
+                    if (mhs.listEmpty() == true) {
+                        cout << "\nList Kosong\n";
+                        break;
+                    }
+                    Node *previous, *current;
+                    cout << endl
+                         << "Masukan no mahasiswa yang di cari : ";
+                    cin >> nim;
+                    if (mhs.search(nim, &previous, &current) == false)
+                        cout << endl
+                             << "Data tidak ditemukan" << endl;
+                    else {
+                        cout << endl
+                             << "Data ditemukan" << endl;
+                        cout << "\nNo Mahasiswa : " << current->noMhs << endl;
+                        cout << "\n";
+                    }
                 }
             }
         }
