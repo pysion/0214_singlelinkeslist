651b15cb3b4169b97407ce0648db257b727b74ea
AdamRaditya <Adamradityaakhsan@gmail.com>
2025-05-06 11:09:00 +0700

    membuat kondisi lain

--- Singly_Linkedlist.cpp
+++ Singly_Linkedlist.cpp
@@ -34,5 +34,23 @@ class LinkedList
             START = nodeBaru;
             return;
         }
-       
+        Node *previous = START;
+        Node *current = START;
+
+        while ((current != NULL) && (nim >= current->noMhs))
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+    }
+
+   
 };
