7b0de22a5acc0207b42e78405fbd6e47b3920fc8
AdamRaditya <Adamradityaakhsan@gmail.com>
2025-05-06 11:23:52 +0700

    membuat kemungkinan delnode

--- Singly_Linkedlist.cpp
+++ Singly_Linkedlist.cpp
@@ -67,6 +67,18 @@ class LinkedList
     }
     
     bool delNode(int nim) {
-       
+        Node *current, *previous;
+        if (!search(nim, &previous, &current))
+        return false;
+
+        if (current == START)
+            START == START->next;
+        else
+            previous->next = current->next;
+
+        delete current;
+        return true;
     }
+    
+   
 };
